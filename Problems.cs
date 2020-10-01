f207776d2eec9b5bd315bc6ac4be6d892b3f4db4
Matthew Arrington <mbarrington@gmail.com>
2020-10-01 08:36:14 -0400

    Implement Problem 7

--- Problems.cs
+++ Problems.cs
@@ -225,5 +225,24 @@ namespace project_euler
 
             return (6, (squareOfSums - sumOfSquares).ToString());
         }
+
+        //--------------------------------------------------
+        // https://projecteuler.net/problem=7
+        //
+        // By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can
+        // see that the 6th prime is 13.
+        //
+        // What is the 10 001st prime number?
+        //--------------------------------------------------
+        // Plan:
+        //  * Reuse earlier function
+        //  * TODO: This took 3.2s... do we need something faster?
+        //--------------------------------------------------
+        public static (int, string) P0007()
+        {
+            var result = Functions.Primes().Take(10001).Last();
+
+            return (7, result.ToString());
+        }
     }
 }
