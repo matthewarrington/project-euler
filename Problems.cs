0786e7a091a1622a0629494345e99dc098559456
Matthew Arrington <mbarrington@gmail.com>
2020-10-01 08:32:14 -0400

    Implement Problem 6

--- Problems.cs
+++ Problems.cs
@@ -194,5 +194,36 @@ namespace project_euler
                 }
             }
         }
+
+        //--------------------------------------------------
+        // https://projecteuler.net/problem=6
+        //
+        // The sum of the squares of the first ten natural numbers is,
+        //
+        //          1^2 + 2^2 + ... + 10^2 = 385
+        //
+        // The square of the sum of the first ten natural numbers is,
+        //
+        //          (1 + 2 + ... + 10)^2 = 55^2 = 3025
+        //
+        // Hence the difference between the sum of the squares of the first ten natural
+        // numbers and the square of the sum is
+        //
+        //          3025 - 385 = 2640
+        //
+        // Find the difference between the sum of the squares of the first one hundred
+        // natural numbers and the square of the sum.
+        //--------------------------------------------------
+        // Plan:
+        //  * Brute force
+        //--------------------------------------------------
+        public static (int, string) P0006()
+        {
+            var max = 100;
+            var sumOfSquares = Enumerable.Range(1, max).Select(x => (int)Math.Pow(x, 2)).Sum();
+            var squareOfSums = (int)Math.Pow(Enumerable.Range(1, max).Sum(), 2);
+
+            return (6, (squareOfSums - sumOfSquares).ToString());
+        }
     }
 }
